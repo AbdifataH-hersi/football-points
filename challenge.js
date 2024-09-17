06988cbbdbbef0d36ffcc441b03ff5f07cbf5f62
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-18 08:03:44 +0900

    football points

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+ function calculatePoints(wins,draws,losses) {
+    
+    let totalPoints = (wins * 3) + (draws * 1) + (losses * 0);
+
+    console.log(`Total points: ${totalPoints}`);
+
+    
+ }
+ calculatePoints(5, 2, 3);
